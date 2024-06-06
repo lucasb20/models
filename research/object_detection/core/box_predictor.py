00fe957c2b7dcd20a17954352678c7338e5cebbe
Lucas Rocha <lr359508@gmail.com>
2024-06-06 19:20:03 -0300

    fix: Layer import changed

--- research/object_detection/core/box_predictor.py
+++ research/object_detection/core/box_predictor.py
@@ -28,7 +28,7 @@ few box predictor architectures are shared across many models.
 """
 from abc import abstractmethod
 import tensorflow.compat.v1 as tf
-from tensorflow.compat.v1.keras.layers import Layer
+from keras.layers import Layer
 
 BOX_ENCODINGS = 'box_encodings'
 CLASS_PREDICTIONS_WITH_BACKGROUND = 'class_predictions_with_background'
